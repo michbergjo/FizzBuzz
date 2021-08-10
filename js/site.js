a23aed8db4360986560759d2b1b717301a2783e6
Michelle Joseph <mbergy27@gmail.com>
2021-08-10 14:56:49 -0500

    Added the code and description to the code page

--- js/site.js
+++ js/site.js
@@ -43,6 +43,52 @@ function fizzBuzz(fizzValue, buzzValue) {
     return values;
 }
 
+function fizzBuzzB(fizzValue, buzzValue) {
+    let returnArray = [];
+    let Fizz = false;
+    let Buzz = false;
+
+    for (let i = 1; i <= 100; i++) {
+       
+        Fizz = i % fizzValue == 0;
+        Buzz = i % buzzValue == 0; 
+
+        switch(true) {
+            case Fizz && Buzz: {
+                returnArray.push("FizzBuzz");
+                break;
+            }
+            case Fizz: {
+                returnArray.push("Fizz");
+                break;
+            }
+            case Buzz: {
+                returnArray.push("Buzz");
+                break;
+            }
+            default: {
+                returnArray.push(i);
+                break;
+            }
+        }
+        
+    }
+
+    return returnArray;
+}
+
+function fizzBuzzC(fizzValue, buzzValue){
+    let returnArray = [];
+
+    for(let i = 1; i <=100; i++){
+
+        let value = ((i % fizzValue == 0 ? "Fizz" : "") + (i % buzzValue == 0 ? "Buzz" : "") || i);
+        returnArray.push(value);
+    }
+
+    return returnArray;
+}
+
 //display the values from fizzbuzz
 function displayValues(values) {
 
